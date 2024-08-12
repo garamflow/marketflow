c5c409f66fbbdfdb6a1b1763743802a3a9ccd7de
garamflow <garamflow@gmail.com>
2024-08-13 01:20:32 +0900

    refactor: props로 input의 모든 속성을 사용할 수 있도록 수정, 에러메시지가 여러 개 보이는 것을 하나만 보이도록 수정

--- app/components/FormInput.tsx
+++ app/components/FormInput.tsx
@@ -1,31 +1,21 @@
+import { InputHTMLAttributes } from "react";
+
 interface FormInputProps {
-	type: string;
-	placeholder: string;
-	required: boolean;
 	name: string;
-	errors: string[];
+	errors?: string[];
 }
 
-const FormInput = ({ type, placeholder, required, name, errors }: FormInputProps) => {
+const FormInput = ({ name, errors = [], ...extraProps }: FormInputProps & InputHTMLAttributes<HTMLInputElement>) => {
 	return (
 		<div className='flex flex-col gap-2 justify-center items-center'>
 			<label className='form-control w-full max-w-xs'>
 				<input
-					type={type}
-					placeholder={placeholder}
-					required={required}
+					{...extraProps}
 					name={name}
 					className='input input-bordered w-full max-w-xs ring-2 focus:ring-4 transition'
 				/>
 				<div className='label'>
-					{errors.map((error, index) => (
-						<span
-							key={index}
-							className='text-red-500 font-medium'
-						>
-							{error}
-						</span>
-					))}
+					<span className='text-red-500 font-medium'>{errors[0]}</span>
 				</div>
 			</label>
 		</div>
