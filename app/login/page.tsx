9a7c522cd0a6c40e03dd691a9acb522011880b97
garamflow <garamflow@gmail.com>
2024-08-15 23:04:56 +0900

    feat: 로그인 페이지에 Zod를 이용한 validation 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,13 @@
+"use client";
 import FormButton from "@/app/components/FormButton";
 import FormInput from "@/app/components/FormInput";
 import SocialLogin from "@/app/components/SocialLogin";
+import { login } from "@/app/login/actions";
+import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from "@/lib/constants";
+import { useFormState } from "react-dom";
 
 const Login = () => {
-	const handleFormSubmit = async (formData: FormData) => {
-		"use server";
-		await new Promise((resolve) => setTimeout(resolve, 5000));
-	};
+	const [state, dispatch] = useFormState(login, null);
 
 	return (
 		<div className='flex flex-col gap-10 py-8 px-5'>
@@ -15,7 +16,7 @@ const Login = () => {
 				<h2 className='text-xl'>이메일로 로그인하기</h2>
 			</div>
 			<form
-				action={handleFormSubmit}
+				action={dispatch}
 				className='flex flex-col'
 			>
 				<FormInput
@@ -23,14 +24,16 @@ const Login = () => {
 					placeholder='이메일'
 					required
 					name='email'
-					errors={[]}
+					errors={state?.fieldErrors.email}
 				/>
 				<FormInput
 					type='password'
 					placeholder='비밀번호'
 					required
 					name='password'
-					errors={[]}
+					minLength={PASSWORD_MIN_LENGTH}
+					maxLength={PASSWORD_MAX_LENGTH}
+					errors={state?.fieldErrors.password}
 				/>
 
 				<FormButton text='로그인' />
