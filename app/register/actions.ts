309bc0d2d114acd97157a479b81e21aff8d32e9b
garamflow <garamflow@gmail.com>
2024-08-20 01:25:41 +0900

    feat: db에서 username과 email이 존재하는 것에 대한 유효성 검사를 zod와 prisma로 구현

--- app/register/actions.ts
+++ app/register/actions.ts
@@ -1,28 +1,62 @@
 "use server";
 import { EMAIL_TYPE_ERROR, INPUT_EMPTY_ERROR, PASSWORD_LENGTH_ERROR, PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR, PASSWORD_TYPE_ERROR } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const checkUsername = (username: string) => !username.includes("#");
 const checkPasswords = ({ password, confirmPassword }: { password: string; confirmPassword: string }) => password === confirmPassword;
 
+const isUsernameAvailable = async (username: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			username,
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	return !Boolean(user);
+};
+
+const isEmailAvailable = async (email: string) => {
+	const userEmail = await db.user.findUnique({
+		where: {
+			email,
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	return !Boolean(userEmail);
+};
+
 const formSchema = z
 	.object({
-		username: z.string({ invalid_type_error: "한글 혹은 영어만 입력 가능합니다.", required_error: INPUT_EMPTY_ERROR }).trim().toLowerCase().refine(checkUsername, "특수문자는 입력 불가능합니다."),
-		email: z.string({ invalid_type_error: EMAIL_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).toLowerCase().email({ message: EMAIL_TYPE_ERROR }),
+		username: z
+			.string({ invalid_type_error: "한글 혹은 영어만 입력 가능합니다.", required_error: INPUT_EMPTY_ERROR })
+			.trim()
+			.toLowerCase()
+			.refine(checkUsername, "특수문자는 입력 불가능합니다.")
+			.refine(isUsernameAvailable, "해당 아이디는 이미 사용중입니다."),
+
+		email: z.string({ invalid_type_error: EMAIL_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).email({ message: EMAIL_TYPE_ERROR }).toLowerCase().refine(isEmailAvailable, "해당 이메일은 이미 존재합니다."),
+
 		password: z.string({ invalid_type_error: PASSWORD_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).min(PASSWORD_MIN_LENGTH, PASSWORD_LENGTH_ERROR).max(PASSWORD_MAX_LENGTH, PASSWORD_LENGTH_ERROR).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+
 		confirmPassword: z.string({ invalid_type_error: PASSWORD_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).min(PASSWORD_MIN_LENGTH, PASSWORD_LENGTH_ERROR).max(PASSWORD_MAX_LENGTH, PASSWORD_LENGTH_ERROR).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 	})
 	.refine(checkPasswords, { message: "비밀번호가 동일하지 않습니다.", path: ["confirmPassword"] });
 
-export const createAccount = (prevState: any, formData: FormData) => {
+export const createAccount = async (prevState: any, formData: FormData) => {
 	const data = { username: formData.get("username") as string | null, email: formData.get("email") as string | null, password: formData.get("password") as string | null, confirmPassword: formData.get("confirmPassword") as string | null };
 
 	if (!data.username || !data.email || !data.password || !data.confirmPassword) error: "모든 필드를 채워주세요.";
 
-	const result = formSchema.safeParse(data);
+	const result = await formSchema.safeParseAsync(data);
 
 	if (!result.success) {
-		console.log(result.error.flatten());
 		return result.error.flatten();
 	}
 };
