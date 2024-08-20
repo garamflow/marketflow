bd560bff8855ff75b1503608808b98396d9939cf
garamflow <garamflow@gmail.com>
2024-08-21 01:42:18 +0900

    feat: 이메일로 로그인 시 이메일 존재 여부, 비밀번호 일치 여부 확인하는 기능 구현

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,10 +1,27 @@
 "use server";
 
 import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const isEmailExists = async (email: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			email,
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	return Boolean(user);
+};
 
 const formSchema = z.object({
-	email: z.string().email().toLowerCase(),
+	email: z.string().email().toLowerCase().refine(isEmailExists, "이메일이 존재하지 않습니다."),
 	password: z.string().min(PASSWORD_MIN_LENGTH).max(PASSWORD_MAX_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
@@ -14,11 +31,34 @@ export const login = async (prevState: any, formData: FormData) => {
 		password: formData.get("password"),
 	};
 
-	const result = formSchema.safeParse(data);
+	const result = await formSchema.safeParseAsync(data);
 
 	if (!result.success) {
 		return result.error.flatten();
 	} else {
-		console.log(result.data);
+		const user = await db.user.findUnique({
+			where: {
+				email: result.data.email,
+			},
+			select: {
+				id: true,
+				password: true,
+			},
+		});
+
+		const comparePassword = await bcrypt.compare(result.data.password, user!.password ?? "");
+
+		if (comparePassword) {
+			const session = await getSession();
+			session.id = user!.id;
+			redirect("/profile");
+		} else {
+			return {
+				fieldErrors: {
+					password: ["잘못된 비밀번호입니다."],
+					email: [],
+				},
+			};
+		}
 	}
 };
