fa65c712d40324bcb6a5b452781a2f901589adee
garamflow <garamflow@gmail.com>
2024-08-21 12:08:41 +0900

    refactor: zod의 superRefine 메소드를 이용해서 사용자명과 이메일이 DB에 존재할 시, 그 다음 단계를 진행하지 않고 에러 메시지를 호출하도록 수정

--- app/register/actions.ts
+++ app/register/actions.ts
@@ -9,46 +9,53 @@ import { redirect } from "next/navigation";
 const checkUsername = (username: string) => !username.includes("#");
 const checkPasswords = ({ password, confirmPassword }: { password: string; confirmPassword: string }) => password === confirmPassword;
 
-const isUsernameAvailable = async (username: string) => {
-	const user = await db.user.findUnique({
-		where: {
-			username,
-		},
-		select: {
-			id: true,
-		},
-	});
-
-	return !Boolean(user);
-};
-
-const isEmailAvailable = async (email: string) => {
-	const userEmail = await db.user.findUnique({
-		where: {
-			email,
-		},
-		select: {
-			id: true,
-		},
-	});
-
-	return !Boolean(userEmail);
-};
-
 const formSchema = z
 	.object({
-		username: z
-			.string({ invalid_type_error: "한글 혹은 영어만 입력 가능합니다.", required_error: INPUT_EMPTY_ERROR })
-			.trim()
-			.toLowerCase()
-			.refine(checkUsername, "특수문자는 입력 불가능합니다.")
-			.refine(isUsernameAvailable, "해당 아이디는 이미 사용중입니다."),
+		username: z.string({ invalid_type_error: "한글 혹은 영어만 입력 가능합니다.", required_error: INPUT_EMPTY_ERROR }).trim().toLowerCase().refine(checkUsername, "특수문자는 입력 불가능합니다."),
 
-		email: z.string({ invalid_type_error: EMAIL_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).email({ message: EMAIL_TYPE_ERROR }).toLowerCase().refine(isEmailAvailable, "해당 이메일은 이미 존재합니다."),
+		email: z.string({ invalid_type_error: EMAIL_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).email({ message: EMAIL_TYPE_ERROR }).toLowerCase(),
 
 		password: z.string({ invalid_type_error: PASSWORD_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).min(PASSWORD_MIN_LENGTH, PASSWORD_LENGTH_ERROR).max(PASSWORD_MAX_LENGTH, PASSWORD_LENGTH_ERROR).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 
 		confirmPassword: z.string({ invalid_type_error: PASSWORD_TYPE_ERROR, required_error: INPUT_EMPTY_ERROR }).min(PASSWORD_MIN_LENGTH, PASSWORD_LENGTH_ERROR).max(PASSWORD_MAX_LENGTH, PASSWORD_LENGTH_ERROR).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+	})
+	.superRefine(async ({ username }, context) => {
+		const user = await db.user.findUnique({
+			where: {
+				username,
+			},
+			select: {
+				id: true,
+			},
+		});
+		if (user) {
+			context.addIssue({
+				code: "custom",
+				message: "해당 아이디는 이미 사용중입니다.",
+				path: ["username"],
+				fatal: true,
+			});
+			return z.NEVER;
+		}
+	})
+	.superRefine(async ({ email }, context) => {
+		const user = await db.user.findUnique({
+			where: {
+				email,
+			},
+			select: {
+				id: true,
+			},
+		});
+		if (user) {
+			context.addIssue({
+				code: "custom",
+				message: "해당 이메일은 이미 사용중입니다.",
+				path: ["email"],
+				fatal: true,
+			});
+			return z.NEVER;
+		}
 	})
 	.refine(checkPasswords, { message: "비밀번호가 동일하지 않습니다.", path: ["confirmPassword"] });
 
